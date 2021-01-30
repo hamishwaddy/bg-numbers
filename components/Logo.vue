ac743829e8a67b229842e1099eacde736fd083a4
hamishwaddy <hamish@swipedon.com>
2021-01-30 19:00:21 +1300

    (#6) replaced logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two" />
-    <div class="Triangle Triangle--one" />
-    <div class="Triangle Triangle--three" />
-    <div class="Triangle Triangle--four" />
-  </div> -->
   <div class="penguin-logo">
     <img src="~/assets/images/peng-01.svg" alt="logo" />
   </div>
